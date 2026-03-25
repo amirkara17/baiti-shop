78e4e16f9edd93e9900af5b1dd9c3f0d569383b1
amir kara <amirkara99@gmail.com>
2026-03-25 17:11:36 +0200

    Update products.js

--- products.js
+++ products.js
@@ -5,7 +5,7 @@ const myProducts = [
         price: 135,
         image: "IMG_8630.jpeg",
         category: "أطقم فناجين ✨",
-        stock: 5 
+        stock: 3
     },
     {
         name: "طقم أكواب الزهور", 
@@ -34,23 +34,23 @@ const myProducts = [
     {
         name: "كوب القطة ماري - ديزني", 
         description: "كوب أصلي من ديزني بتصميم القطة ماري اللطيفة.",
-        price: 50, 
+        price: 35, 
         image: "6851461d-69ed-4f13-b9c2-21de70672611.jpeg", 
         category: "ديزني الأصلي 🪄",
-        stock: 10 
+        stock: 1 
     },
     {
         name: "كوب ستيتش - ديزني", 
         description: "كوب أصلي من ديزني بتصميم ستيتش.",
-        price: 50, 
+        price: 35, 
         image: "52fe9b36-8146-43d3-9bb6-4c627900138d.jpeg", 
         category: "ديزني الأصلي 🪄",
-        stock: 10
+        stock: 1
     },
     {
         name: "كوب ميني ماوس - ديزني", 
         description: "كوب أصلي من ديزني بتصميم ميني ماوس.",
-        price: 50, 
+        price: 35, 
         image: "d74466ca-a951-439e-b766-5a0f38c69da6.jpeg", 
         category: "ديزني الأصلي 🪄",
         stock: 1
