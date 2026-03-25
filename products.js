8882d2a2f3a5a52d0d998dcc3c759917c10d862d
amir kara <amirkara99@gmail.com>
2026-03-25 12:48:05 +0200

    Update products.js

--- products.js
+++ products.js
@@ -4,34 +4,39 @@ const myProducts = [
         description: "طقم مميز وأنيق يضيف لمسة دافئة لضيافتك ولبيتك.",
         price: 170,
         image: "IMG_8630.jpeg",
-        category: "أطقم فناجين ✨" // תוקן השם
+        category: "أطقم فناجين ✨",
+        stock: 5 // יש 5 במלאי
     },
     {
         name: "أكواب بتصاميم مميزة", 
         description: "أكواب جميلة برسومات لطيفة (السعر للكوب الواحد).",
         price: 35,
         image: "IMG_8631.jpeg",
-        category: "أكواب ☕"
+        category: "أكواب ☕",
+        stock: 2 // נשארו רק 2! יקפוץ כיתוב מיוחד
     },
     {
         name: "كوب القطة ماري - ديزني", 
         description: "كوب أصلي من ديزني بتصميم القطة ماري اللطيفة.",
         price: 50, 
         image: "6851461d-69ed-4f13-b9c2-21de70672611.jpeg", 
-        category: "ديزني الأصلي 🪄"
+        category: "ديزني الأصلي 🪄",
+        stock: 0 // המלאי נגמר! אי אפשר להזמין
     },
     {
         name: "كوب ستيتش - ديزني", 
         description: "كوب أصلي من ديزني بتصميم ستيتش.",
         price: 50, 
         image: "52fe9b36-8146-43d3-9bb6-4c627900138d.jpeg", 
-        category: "ديزني الأصلي 🪄"
+        category: "ديزني الأصلي 🪄",
+        stock: 10
     },
     {
         name: "كوب ميني ماوس - ديزني", 
         description: "كوب أصلي من ديزني بتصميم ميني ماوس.",
         price: 50, 
         image: "d74466ca-a951-439e-b766-5a0f38c69da6.jpeg", 
-        category: "ديزني الأصلي 🪄"
+        category: "ديزني الأصلي 🪄",
+        stock: 1
     }
 ];
