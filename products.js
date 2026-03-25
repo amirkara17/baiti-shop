6987c73537a8666a3c28c146897b13a90b6cbfab
amir kara <amirkara99@gmail.com>
2026-03-25 12:55:04 +0200

    Update products.js

--- products.js
+++ products.js
@@ -18,23 +18,23 @@ const myProducts = [
     {
         name: "كوب القطة ماري - ديزني", 
         description: "كوب أصلي من ديزني بتصميم القطة ماري اللطيفة.",
-        price: 50, 
+        price: 35, 
         image: "6851461d-69ed-4f13-b9c2-21de70672611.jpeg", 
         category: "ديزني الأصلي 🪄",
-        stock: 0 // המלאי נגמר! אי אפשר להזמין
+        stock: 1 // המלאי נגמר! אי אפשר להזמין
     },
     {
         name: "كوب ستيتش - ديزني", 
         description: "كوب أصلي من ديزني بتصميم ستيتش.",
-        price: 50, 
+        price: 35, 
         image: "52fe9b36-8146-43d3-9bb6-4c627900138d.jpeg", 
         category: "ديزني الأصلي 🪄",
-        stock: 10
+        stock: 1
     },
     {
         name: "كوب ميني ماوس - ديزني", 
         description: "كوب أصلي من ديزني بتصميم ميني ماوس.",
-        price: 50, 
+        price: 35, 
         image: "d74466ca-a951-439e-b766-5a0f38c69da6.jpeg", 
         category: "ديزني الأصلي 🪄",
         stock: 1
