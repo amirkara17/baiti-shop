7bb0548c5a91a4b4a3341948d2dbab3d7ff9d25e
amir kara <amirkara99@gmail.com>
2026-03-25 13:35:35 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,11 +1,27 @@
 const myProducts = [
     {
-        name: "طقم الفراشات", 
-        description: "طقم فناجين للقهوة مميز وأنيق",
-        price: 135,
+        name: "طقم فاخر", 
+        description: "طقم مميز وأنيق يضيف لمسة دافئة لضيافتك ولبيتك.",
+        price: 170,
         image: "IMG_8630.jpeg",
         category: "أطقم فناجين ✨",
-        stock: 3 // יש 5 במלאי
+        stock: 5 
+    },
+    {
+        name: "طقم أكواب الزهور", 
+        description: "طقم رائع بتصميم الزهور البارزة، يضيف لمسة ربيعية لضيافتك.",
+        price: 140,
+        image: "1d443a16-2a6a-4af8-a790-16fca67a85c6.jpeg",
+        category: "أطقم فناجين ✨",
+        stock: 0 // אזל מהמלאי
+    },
+    {
+        name: "طقم القلعة الكلاسيكي", 
+        description: "طقم ساحر بتصميم القلعة والعربة الكلاسيكية، يأتيك في علبة هدايا فاخرة.",
+        price: 99,
+        image: "1109cc14-59e1-4bf8-830a-94966e0a6b36.jpeg",
+        category: "تشكيلة القلعة 🏰", // הקטגוריה החדשה
+        stock: 0 // אזל מהמלאי
     },
     {
         name: "أكواب بتصاميم مميزة", 
@@ -13,28 +29,28 @@ const myProducts = [
         price: 35,
         image: "IMG_8631.jpeg",
         category: "أكواب ☕",
-        stock: 2 // נשארו רק 2! יקפוץ כיתוב מיוחד
+        stock: 2
     },
     {
         name: "كوب القطة ماري - ديزني", 
         description: "كوب أصلي من ديزني بتصميم القطة ماري اللطيفة.",
-        price: 35, 
+        price: 50, 
         image: "6851461d-69ed-4f13-b9c2-21de70672611.jpeg", 
         category: "ديزني الأصلي 🪄",
-        stock: 1 // המלאי נגמר! אי אפשר להזמין
+        stock: 10 
     },
     {
         name: "كوب ستيتش - ديزني", 
         description: "كوب أصلي من ديزني بتصميم ستيتش.",
-        price: 35, 
+        price: 50, 
         image: "52fe9b36-8146-43d3-9bb6-4c627900138d.jpeg", 
         category: "ديزني الأصلي 🪄",
-        stock: 1
+        stock: 10
     },
     {
         name: "كوب ميني ماوس - ديزني", 
         description: "كوب أصلي من ديزني بتصميم ميني ماوس.",
-        price: 35, 
+        price: 50, 
         image: "d74466ca-a951-439e-b766-5a0f38c69da6.jpeg", 
         category: "ديزني الأصلي 🪄",
         stock: 1
