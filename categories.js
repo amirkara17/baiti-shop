3b5a1d161483a9509a948569898a01147b6355b1
amir kara <amirkara99@gmail.com>
2026-03-26 15:51:22 +0200

    Update categories.js

--- categories.js
+++ categories.js
@@ -1,6 +1,1 @@
-const myCategories = [
-    { name: "أطقم فناجين ✨", icon: "☕" },
-    { name: "تشكيلة القلعة 🏰", icon: "🏰" },
-    { name: "أكواب ☕", icon: "☕" },
-    { name: "ديزني الأصلي 🪄", icon: "🪄" }
-];
+const myCategories = [];
