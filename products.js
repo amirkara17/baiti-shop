7b117dedc6175b1f7cf24c2a684cd9d3a4095b04
amir kara <amirkara99@gmail.com>
2026-03-29 23:37:04 +0300

    Update products.js

--- products.js
+++ products.js
@@ -10,7 +10,7 @@ const myProducts = [
             "IMG_8749.jpeg",
             "IMG_8748.jpeg"
         ],
-        "category": "شموع عطرية 🕯️",
+        "category": "شموع عطرية",
         "stock": 3
     },
     {
@@ -25,7 +25,7 @@ const myProducts = [
             "4d50f1a0-1acb-468e-a1fa-bcd081afdda0.jpeg",
             "f2d8238d-6358-4d58-9378-28d98cf72b79.jpeg"
         ],
-        "category": "شموع عطرية 🕯️",
+        "category": "شموع عطرية",
         "stock": 5
     },
     {
@@ -34,7 +34,7 @@ const myProducts = [
         "description": "",
         "price": 35,
         "image": "ebfa1560-eba2-4c2b-a8e8-7ce53eb2a620.jpeg",
-        "category": "ديزني الأصلي 🪄",
+        "category": "ديزني الأصلي",
         "stock": 0,
         "oldPrice": 45
     },
@@ -43,7 +43,7 @@ const myProducts = [
         "description": "طقم فناجين للقهوة مميز وأنيق.",
         "price": 170,
         "image": "7c584c2a-b25c-4b35-baaf-dfd31266b9c6.jpeg",
-        "category": "أطقم فناجين ✨",
+        "category": "أطقم فناجين",
         "stock": 1,
         "images": [
             "7c584c2a-b25c-4b35-baaf-dfd31266b9c6.jpeg",
@@ -57,7 +57,7 @@ const myProducts = [
         "description": "طقم رائع بتصميم الزهور البارزة، يضيف لمسة ربيعية لضيافتك.",
         "price": 170,
         "image": "1109cc14-59e1-4bf8-830a-94966e0a6b36.jpeg",
-        "category": "أطقم فناجين ✨",
+        "category": "أطقم فناجين",
         "stock": 0,
         "oldPrice": 180
     },
@@ -66,7 +66,7 @@ const myProducts = [
         "description": "طقم ساحر بتصميم القلعة والعربة الكلاسيكية، يأتيك في علبة هدايا فاخرة.",
         "price": 99,
         "image": "1d443a16-2a6a-4af8-a790-16fca67a85c6.jpeg",
-        "category": "تشكيلة القلعة 🏰",
+        "category": "تشكيلة القلعة",
         "stock": 0
     },
     {
@@ -74,7 +74,7 @@ const myProducts = [
         "description": "أكواب جميلة برسومات لطيفة (السعر للكوب الواحد).",
         "price": 35,
         "image": "IMG_8631.jpeg",
-        "category": "أكواب ☕",
+        "category": "أكواب",
         "stock": 0
     },
     {
@@ -82,7 +82,7 @@ const myProducts = [
         "description": "كوب أصلي من ديزني بتصميم القطة ماري اللطيفة.",
         "price": 30,
         "image": "6851461d-69ed-4f13-b9c2-21de70672611.jpeg",
-        "category": "ديزني الأصلي 🪄",
+        "category": "ديزني الأصلي",
         "stock": 1,
         "oldPrice": 35
     },
@@ -91,7 +91,7 @@ const myProducts = [
         "description": "كوب أصلي من ديزني بتصميم ستيتش.",
         "price": 35,
         "image": "52fe9b36-8146-43d3-9bb6-4c627900138d.jpeg",
-        "category": "ديزني الأصلي 🪄",
+        "category": "ديزني الأصلي",
         "stock": 1
     },
     {
@@ -99,7 +99,7 @@ const myProducts = [
         "description": "كوب أصلي من ديزني بتصميم ميني ماوس.",
         "price": 35,
         "image": "d74466ca-a951-439e-b766-5a0f38c69da6.jpeg",
-        "category": "ديزني الأصلي 🪄",
+        "category": "ديزني الأصلي",
         "stock": 1
     }
 ];
